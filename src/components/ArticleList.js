05f9203437ec442188b4327b3faa5f5028e2e052
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-03-15 11:12:42 +0200

    add oneOpen HOC

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,31 +1,27 @@
 import React, { Component, PropTypes } from 'react'
 import Article from './Article'
-import openOnlyOne from '../HOC/openOnlyOne'
+import CommentList from './CommentList'
+import oneOpen from '../HOC/oneOpen'
 
-const ArticleList=(props)=>{
-
-        const {handleOpen, isOpen}=props;
-
-        const articles = props.articles.map((article) =>
-
-            <div className='pure-u-1 pure-u-md-1-3' key={article.id}>
+class ArticleList extends Component {
+    render() {
+        const { articles, isItemOpen, openItem } = this.props
+        const articleItems = articles.map((article) =>
+            <li key={article.id}>
                 <Article article={article}
-                         openArticle = {handleOpen(article.id)}
-                         isOpen = {isOpen(article.id)}/>
-            </div>
-        );
-
+                         openArticle = {openItem(article.id)}
+                         isOpen = {isItemOpen(article.id)}/>
+            </li>
+        )
         return (
-            <div className='pure-g'>
-
-                    {articles}
-
-
+            <div>
+                <ul>
+                    {articleItems}
+                </ul>
             </div>
         )
     }
 
+}
 
-
-
-export default openOnlyOne(ArticleList)
+export default oneOpen(ArticleList)
