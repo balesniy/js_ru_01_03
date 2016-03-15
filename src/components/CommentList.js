b7dc4888545dddf680b6c279b1f7ee6dbdfc33a8
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-03-15 11:19:15 +0200

    add linkedState mixin

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,24 +1,38 @@
 import React, { Component, PropTypes } from 'react'
 import Comment from './Comment'
-import AddCommentForm from './addCommentForm'
 import toggleOpen from '../HOC/toggleOpen'
+import linkedState from 'react-addons-linked-state-mixin'
 
-const CommentList=(props)=> {
-        const { isOpen, comments, toggleOpen } = props;
+const CommentList = React.createClass({
+    mixins: [linkedState],
+    propTypes: {
+        comments: PropTypes.array
+    },
+    getInitialState() {
+        return {
+            comment: ''
+        }
+    },
+    render() {
+        const { isOpen, comments, toggleOpen } = this.props
+        const actionText = isOpen ? 'hide comments' : 'show comments'
 
-        const actionText = isOpen ? 'hide comments' : 'show comments';
-
-        const commentItems = comments.map((comment) =>
-            <Comment key={comment.id} comment = {comment}/>);
-
+        const commentItems = comments.map((comment) => <li key={comment.id}><Comment comment = {comment}/></li>)
         return (
             <div>
-                <button className="button-success pure-button" onClick = {toggleOpen}>{actionText}</button>
-                {isOpen ? commentItems : null}
-                <AddCommentForm/>
+                <a href = "#" onClick = {toggleOpen}>{actionText}</a>
+                <ul>{isOpen ? commentItems : null}</ul>
+                {this.getInput()}
             </div>
         )
-};
-
+    },
+    getInput() {
+        if (!this.props.isOpen) return null
+        return <div>
+            <input valueLink={this.linkState("comment")}/>
+            <a href = "#">add comment</a>
+        </div>
+    }
+})
 
 export default toggleOpen(CommentList)
