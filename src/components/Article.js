e09844db346dabea4b1ee91f737b16d84e003f4e
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-03-15 19:27:36 +0200

    add full flux cycle

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,20 +1,47 @@
 import React, { Component, PropTypes } from 'react'
-import Body from './Body'
+import {findDOMNode} from 'react-dom'
+import CommentList from './CommentList'
+import { deleteArticle } from '../actions/articles'
 
+class Article extends Component {
+    static propTypes = {
+        isOpen: PropTypes.bool,
+        article: PropTypes.object.isRequired
+    }
 
-
-export default (props)=> {
-
-    const { article: { title }, openArticle, isOpen, article } = props;
+    render() {
         return (
-            <div>
-                <h3 onClick={openArticle} className="content-subhead">
-                    {title}
-                </h3>
-                <Body article={article} isOpen={isOpen}/>
+            <div ref="container">
+                <a href = "#" onClick = {this.handleDelete}>delete</a>
+                {this.getTitle()}
+                {this.getBody()}
             </div>
         )
     }
 
+    handleDelete = (ev) => {
+        ev.preventDefault()
+        deleteArticle(this.props.article.id)
+    }
 
+    getBody() {
+        const { article, isOpen } = this.props
+        if (!isOpen) return <noscript />
+        return (
+            <div>
+                <p>{article.text}</p>
+                <CommentList ref= "comments" comments = {article.comments || []} />
+            </div>
+        )
+    }
+    getTitle() {
+        const { article: { title }, openArticle  } = this.props
+        return  (
+            <h3 onClick={openArticle}>
+                {title}
+            </h3>
+        )
+    }
+}
 
+export default Article
