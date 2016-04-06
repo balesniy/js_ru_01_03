992e7c77364861ffb96e5c0f52c8f0ccf906cdb2
balesniy <balesniy@yandex.ru>
2016-04-06 18:41:48 +0300

    redux-delete article

--- src/containers/Articles.js
+++ src/containers/Articles.js
@@ -1,17 +1,30 @@
 import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+import { deleteArticle } from '../actions/articles.js'
 
 class Articles extends Component {
     static propTypes = {
+        articles: PropTypes.array,
+        deleteArticle: PropTypes.func
 
     };
 
     render() {
+        const { articles, deleteArticle } = this.props
         return (
-            <div>
-                <h1>Implement Me</h1>
-            </div>
+            <ul>
+                {articles.map(item=>{return (
+                    <li key={item.id}>
+                        {item.title}
+                        <a href="#" onClick={()=>deleteArticle(item.id)}>delete</a>
+                    </li>)
+                })}
+            </ul>
         )
     }
 }
 
-export default Articles
+export default connect((state) => {
+    const { articles } = state
+    return { articles }
+},{deleteArticle})(Articles)
--- src/ruducers/articles.js
+++ src/ruducers/articles.js
@@ -1,12 +1,14 @@
-import {  } from '../constants'
+import { DELETE_ARTICLE } from '../constants'
 import { articles as defaultArticles} from '../fixtures'
 
 export default (articles = defaultArticles, action) => {
     const { type, data } = action
 
     switch (type) {
+        case DELETE_ARTICLE: return articles.filter(item=>item.id!=data)
+        default: return articles
 
     }
 
-    return articles
+
 }
